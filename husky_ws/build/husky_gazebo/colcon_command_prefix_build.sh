e43469928b5e2aa57db26b8f2d434b890247a922
Eylul OZER <49402359+eylulzer@users.noreply.github.com>
2023-01-11 10:01:49 +0300

    husky_ws eklendi

---
+++ husky_ws/build/husky_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ali/husky_ws/install/husky_description/share/husky_description/package.sh"
+. "/home/ali/husky_ws/install/husky_control/share/husky_control/package.sh"
